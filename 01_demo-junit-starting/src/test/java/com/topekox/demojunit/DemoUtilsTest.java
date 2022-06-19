0fc8bf2fd612f42f256d31e3e69ba5a8a8d7fa4c
Ucup Topekox <ucup.topekox@gmail.com>
2022-06-20 00:07:46 +0800

    Test lifecycle

--- 01_demo-junit-starting/src/test/java/com/topekox/demojunit/DemoUtilsTest.java
+++ 01_demo-junit-starting/src/test/java/com/topekox/demojunit/DemoUtilsTest.java
@@ -1,14 +1,39 @@
 package com.topekox.demojunit;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DemoUtilsTest {
 
+    DemoUtils demoUtils;
+
+    @BeforeEach
+    void beforeEach() {
+        demoUtils = new DemoUtils();
+        System.out.println("@BeforeEach di eksekusi sebelum memanggil method test");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("@AfterEach di eksekusi sesudah memanggil method test");
+        System.out.println();
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("@BeforeAll harus static: dipanggil sekali sebelum eksekusi semua method test");
+        System.out.println();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("@AfterAll harus static: dipanggil sekali setelah eksekusi semua method test");
+    }
+
     @Test
     void testEqualsAndNotEquals() {
-        DemoUtils demoUtils = new DemoUtils();
+        System.out.println("Running Test: testEqualsAndNotEquals()");
 
         assertEquals(6, demoUtils.add(1, 5), "1 + 5 must be 6");
         assertNotEquals(9, demoUtils.add(5, 1), "5 + 1 must be not 6");
@@ -16,7 +41,7 @@ public class DemoUtilsTest {
 
     @Test
     void testNullAndNotNull() {
-        DemoUtils demoUtils = new DemoUtils();
+        System.out.println("Running Test: testNullAndNotNull()");
 
         String a = null;
         String b = "topekox";

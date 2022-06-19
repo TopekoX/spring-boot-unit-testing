6e75e77b717a95c4436a3a350961c6540b2216d6
Ucup Topekox <ucup.topekox@gmail.com>
2022-06-20 00:12:49 +0800

    display name method test

--- 01_demo-junit-starting/src/test/java/com/topekox/demojunit/DemoUtilsTest.java
+++ 01_demo-junit-starting/src/test/java/com/topekox/demojunit/DemoUtilsTest.java
@@ -14,40 +14,38 @@ public class DemoUtilsTest {
         System.out.println("@BeforeEach di eksekusi sebelum memanggil method test");
     }
 
-    @AfterEach
-    void afterEach() {
-        System.out.println("@AfterEach di eksekusi sesudah memanggil method test");
-        System.out.println();
-    }
-
-    @BeforeAll
-    static void beforeAll() {
-        System.out.println("@BeforeAll harus static: dipanggil sekali sebelum eksekusi semua method test");
-        System.out.println();
-    }
-
-    @AfterAll
-    static void afterAll() {
-        System.out.println("@AfterAll harus static: dipanggil sekali setelah eksekusi semua method test");
-    }
-
     @Test
+    @DisplayName("Equals and Not Equals")
     void testEqualsAndNotEquals() {
-        System.out.println("Running Test: testEqualsAndNotEquals()");
-
         assertEquals(6, demoUtils.add(1, 5), "1 + 5 must be 6");
         assertNotEquals(9, demoUtils.add(5, 1), "5 + 1 must be not 6");
     }
 
     @Test
+    @DisplayName("Null and Not Null")
     void testNullAndNotNull() {
-        System.out.println("Running Test: testNullAndNotNull()");
-
         String a = null;
         String b = "topekox";
 
         assertNull(demoUtils.checkNull(a));
         assertNotNull(demoUtils.checkNull(b));
     }
+
+//    @AfterEach
+//    void afterEach() {
+//        System.out.println("@AfterEach di eksekusi sesudah memanggil method test");
+//        System.out.println();
+//    }
+//
+//    @BeforeAll
+//    static void beforeAll() {
+//        System.out.println("@BeforeAll harus static: dipanggil sekali sebelum eksekusi semua method test");
+//        System.out.println();
+//    }
+//
+//    @AfterAll
+//    static void afterAll() {
+//        System.out.println("@AfterAll harus static: dipanggil sekali setelah eksekusi semua method test");
+//    }
 
 }
